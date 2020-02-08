1e39f89dde9b776946fb02da924f3a81f9921eb0
Ruchi <44723987+rappinngu@users.noreply.github.com>
2020-02-08 16:19:58 +0900

    余剰の追加

--- Assets/Scripts/Test01.cs
+++ Assets/Scripts/Test01.cs
@@ -5,10 +5,16 @@ public class Test01 : MonoBehaviour
     void Start()
     {
         Debug.Log(Multiply(double.NegativeInfinity, double.PositiveInfinity));
+        Debug.Log($"surplus = {surplus(3, 2)}");
     }
 
     double Multiply(double a, double b)
     {
         return a * b;
+    }
+
+    int surplus(int a, int b)
+    {
+        return a % b;
     }
 }
