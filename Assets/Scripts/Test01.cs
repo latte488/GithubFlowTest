66c2159857e528804015a7ce0b8c550ce23e67b7
latte488 <latte488@icloud.com>
2020-02-08 12:09:42 +0900

    7

--- Assets/Scripts/Test01.cs
+++ Assets/Scripts/Test01.cs
@@ -6,6 +6,7 @@ public class Test01 : MonoBehaviour
     {
         Debug.Log(Multiply(double.NegativeInfinity, double.PositiveInfinity));
         Debug.Log(surplus(3,2));
+        Debug.Log($"Add(1, 2) = {Add(1, 2)}");
     }
 
     double Subtract(double a,double b)
@@ -18,8 +19,13 @@ public class Test01 : MonoBehaviour
         return a * b;
     }
 
-    double surplus(double a,double b)
+    double surplus(double a, double b)
     {
         return a % b;
+    }
+
+    int Add(int a, int b)
+    {
+        return a + b;
     }
 }
