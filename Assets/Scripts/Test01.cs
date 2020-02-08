ff6bea7e1a695788e9ce81470cf5559b2c38aad7
latte488 <latte488@icloud.com>
2020-02-08 12:09:42 +0900

    7

---
+++ Assets/Scripts/Test01.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log($"Add(1, 2) = {Add(1, 2)}");
+    }
+
+    int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
