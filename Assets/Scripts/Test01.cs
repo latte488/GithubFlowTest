05fe4d7da050a08ace7856946f956bedf27bf0c1
ラッピングルチ <rappinngu2006@gmail.com>
2020-02-09 09:44:38 +0900

    Merge branch 'surplus' of https://github.com/latte488/GithubFlowTest into surplus
    # Conflicts:
    #	Assets/Scenes/SampleScene.unity
    #	Assets/Scripts/Test01.cs

--- Assets/Scripts/Test01.cs
+++ Assets/Scripts/Test01.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Test01 : MonoBehaviour
 {
     void Start()
     {
+        Debug.Log(Multiply(double.NegativeInfinity, double.PositiveInfinity));
         Debug.Log($"Add(1, 2) = {Add(1, 2)}");
+        Debug.Log("surplus = " + surplus(3, 2));
+    }
+
+    double Multiply(double a, double b)
+    {
+        return a * b;
     }
 
     int Add(int a, int b)
     {
         return a + b;
+    }
+
+    int surplus(int a, int b)
+    {
+        return a % b;
     }
 }
